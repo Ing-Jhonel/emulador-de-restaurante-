969005ba59f7594711ff61f421f252492e42f3a2
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-14 23:53:43 -0500

    Avance 2

--- Interfaz basica del emulador.cpp
+++ Interfaz basica del emulador.cpp
@@ -30,18 +30,22 @@ int anchoConsola() {
     return columnas;
 }
 
+void imprimirEspacio(int n){
+	for(int i=0; i<n; i++){
+		cout << " ";
+	}
+}
 // Función para centrar el texto
 void centrar(const string& text) {
     int tamanoconsola = anchoConsola(); // ya que hallamos columnas con la funcion, le asignamos una variable al retorno
     int tamanotexto = text.length(); //"text.lenght()" halla el tamaño del texto en caracteres. le asignamos una variable de tipo int a la anchura del texto
     int espaciado = (tamanoconsola - tamanotexto) / 2; // la diferencia de ambos entre 2 es la distancia para centrar un texto
-
     // Imprimir espacios en blanco antes del texto
-    for (int i = 0; i < espaciado; ++i) {
-        cout << " ";
-    }
+    imprimirEspacio(espaciado);
     // Imprimir el texto
     cout << text;
+    
+    imprimirEspacio(espaciado);
     
 }
 
@@ -49,11 +53,8 @@ void centrarCin(int tamanotexto){
 
     int tamanoconsola = anchoConsola(); // ya que hallamos columnas con la funcion, le asignamos una variable al retorno
     int espaciado = (tamanoconsola - tamanotexto) / 2; // la diferencia de ambos entre 2 es la distancia para centrar un texto
-
     // Imprimir espacios en blanco antes del texto
-    for (int i = 0; i < espaciado; ++i) {
-        cout << " ";
-    }
+    imprimirEspacio(espaciado);
 }
 
 void separador(char c){
@@ -71,25 +72,20 @@ void enjaular(string text, char c){
 	int tamanoconsola = anchoConsola();
 	int espaciado = (tamanoconsola - anchojaula)/2;
 	//largo de jaula es 3
-
-	for(int i=0; i<espaciado; i++){
-		cout << " ";
-	}
+	imprimirEspacio(espaciado);
+	
 	for(int i=0; i<anchojaula; i++){
 		cout << c;
 	}
 	cout << endl;
 	
-	for(int i=0; i<espaciado; i++){
-		cout << " ";
-	}
+	imprimirEspacio(espaciado);
 	
 	cout << c << " " << text << " " << c;
 	cout << endl;
 	
-	for(int i=0; i<espaciado; i++){
-		cout << " ";
-	}
+	imprimirEspacio(espaciado);
+	
 	for(int i=0; i<anchojaula; i++){
 		cout << c;
 	}
@@ -103,23 +99,96 @@ void subrayar(string text, char c){
 	int tamanoconsola = anchoConsola();
 	int tamanotexto = text.length();
 	int espaciado = (tamanoconsola - tamanotexto)/2;
-	for(int i=0; i<espaciado; i++){
-		cout << " ";
-	}
+	
+	imprimirEspacio(espaciado);
+	
 	for(int i=0; i<tamanotexto; i++){
 		cout << c;
 	}
 
 }
 
+void linea(int n, char s, int salto){
+	int tamanoconsola = anchoConsola();
+	int espaciado = (tamanoconsola - n)/2;
+	
+	imprimirEspacio(espaciado);
+	
+	for(int i=0; i<n; i++){
+		cout << s;
+	}
+	
+	 if(salto>0){
+		cout << endl;
+		
+		imprimirEspacio(espaciado);
+		
+		cout << s;
+		int espaciojaula = n-2;
+		
+		imprimirEspacio(espaciojaula);
+		
+		cout << s;
+	} 
+}
+
+void contenidoJaula(string text, int n, char s, int salto){
+	int tamanoconsola = anchoConsola();
+	int espaciado = (tamanoconsola - n)/2;
+	
+	imprimirEspacio(espaciado);
+	
+	cout << s;
+	
+	int tamanotexto = text.length();
+	int entrelineas = n-2-tamanotexto;
+	int entrelinea1 = (n-2-tamanotexto)/2;
+	int entrelinea2 = entrelineas - entrelinea1;
+	
+	imprimirEspacio(entrelinea1);
+	
+	cout << text;
+
+	imprimirEspacio(entrelinea2);
+	
+	cout << s;
+	
+	imprimirEspacio(espaciado);
+	
+	if(salto!=0){
+		for(int i=0; i<salto; i++){
+			cout << endl;
+			
+			imprimirEspacio(espaciado);
+			
+			cout << s;
+			
+			int espaciojaula=n-2;
+			
+			imprimirEspacio(espaciojaula);
+			
+			cout << s;
+		}
+	}
+}
+
+struct PlatosDeComida{
+	string nombre;
+	string ingredientes;
+	float precio;
+	float costo;
+	int cantidad;
+};
 int main(){
 	
 	system("pause");
-
 	setlocale(LC_ALL, ""); //parte de la liberia locale.h
+	
 	string texto;
 	char s;
 	int modalidad;
+	int max= 100;
+	PlatosDeComida comida[max];
 	do{
 		color(hConsole, 79);
 		cout << endl;
@@ -130,7 +199,7 @@ int main(){
 		s = '*';
 		separador(s); cout << endl;
 		
-		color(hConsole, 224);
+		color(hConsole, 7);
 		cout << endl << endl;
 		texto = "Elije el modo de juego";
 		s = '*';
@@ -141,20 +210,26 @@ int main(){
 		s= '=';
 		subrayar(texto, s); cout << endl << endl;
 		
+		int anchoJaula=40;
+		int salto=2;
+		s='*';
+		linea(anchoJaula, s, salto); cout << endl;
 		texto = "1. DUEÑO";
-		centrar(texto); cout << endl << endl;
+		contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
 		texto = "2. CLIENTE";
-		centrar(texto); cout << endl << endl;
+		contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
 		texto = "3. DELIVERY";
-		centrar(texto); cout << endl << endl;
+		contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
 		texto = "4. ABANDONAR EMULADOR";
-		centrar(texto); cout << endl << endl;
+		contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
+		linea(anchoJaula, s, 0); cout << endl;
 		
 		texto = "---------------";
 		centrar(texto); cout << endl;
 		centrarCin(1);
 		cin >> modalidad;
-		
+		cout << endl;
+
 		char opcion;
 		switch(modalidad){
 			case 1:
@@ -164,17 +239,76 @@ int main(){
 				texto = "¿Que quieres hacer?";
 				centrar(texto); cout << endl << endl;
 				do{
-					texto = "A) Administrar cocina";
+					texto = "A) Administrar la cocina";
 					centrar(texto); cout << endl << endl;
+					texto = "B) Administrar el negocio";
+					centrar(texto); cout << endl << endl;
+					texto = "C) Administrar personal";
+					centrar(texto); cout << endl << endl;
+					texto = "D) Cambiar de rol";
+					centrar(texto); cout << endl << endl;
+					texto = "--------";
+					centrar(texto); cout << endl;
+					centrarCin(1);
 					cin >> opcion;
-				} while(opcion == 'E');
+					
+					cout << endl;
+					
+					switch (opcion){
+						case 'A':
+							texto = "Entrando a la cocina...";
+							centrar(texto); cout << endl << endl;
+							texto = "Indique la accion que va a realizar";
+							centrar(texto);
+							s= '=';
+							subrayar(texto, s); cout << endl << endl;
+							do{
+								anchoJaula = 40;
+								salto = 0;
+								s = '*';
+								linea(anchoJaula, s, 2); cout << endl;
+								texto = "a) Agregar una comida";
+								contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
+								texto = "b) Eliminar una comida";
+								contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
+								texto = "c) Actualizar una comida";
+								contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
+								texto = "d) Ver stock del menú";
+								contenidoJaula(texto, anchoJaula, s, salto); cout << endl;
+								texto = "e) Salir de la cocina";
+								contenidoJaula(texto, anchoJaula, s, 1); cout << endl;
+								linea(anchoJaula, s, 0); cout << endl << endl;
+								texto = "--------";
+								centrar(texto); cout << endl;
+								centrarCin(1);
+								cin >> opcion;
+								
+								int n=0;
+								switch(opcion){
+									case 'a':
+										cout << "\t\t\t\t\t\t Nombre: ";
+										getline(cin, comida[n].nombre);
+										cout << "\t\t\t\t\t\t ";
+										break;
+								}
+							} while(opcion!='e');
+							break;
+						case 'B':
+							break;
+						case 'C':
+							break;
+						default:
+							break;
+					}
+				} while(opcion != 'D');
+				
 				break;
 				
 			case '2':
 				break;
 			case '3':
 				break;
-			Default:
+			default:
 				cout << "Indique el numero de las modalidades disponibles.";
 				break;
 		}
